89d3715d828f1a48c0e70096f4d51337c20fda3c
alexgrand <namelost@protonmail.com>
2019-05-10 19:04:55 +0800

    Books and Study

---
+++ Big English/Lvl 1/Teacher's book/Big English (British English) 1 Teacher's Book_files/MxSPmId3B8L.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["YWMVW"]); }
+
+__d("getContentEditableContainer",["invariant","ReactDOM"],(function(a,b,c,d,e,f,g){"use strict";function a(a){a=b("ReactDOM").findDOMNode(a.editorContainer);a||g(0,4998);a.firstChild instanceof HTMLElement||g(0,4999);return a.firstChild}e.exports=a}),null);
